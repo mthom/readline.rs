96c516ff8d021f4cc467d6bc71f6e1935bea40dd
Mark Thom <markjordanthom@gmail.com>
2019-03-17 18:42:00 -0600

    add library search paths

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    println!("cargo:rustc-link-search=native=/lib");
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    
     println!("cargo:rustc-link-lib=readline");
 }
